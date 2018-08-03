2e6fe6a5a5347509d743668100eb2e831cfe0006
Ryan Wisnesky <wisnesky@gmail.com>
2018-08-02 23:30:33 -0400

    clean up, add show and eq instances

--- app/Main.hs
+++ app/Main.hs
@@ -1,66 +1,76 @@
-{-# LANGUAGE ExistentialQuantification, ExplicitForAll, StandaloneDeriving, DuplicateRecordFields,ScopedTypeVariables,InstanceSigs, KindSignatures, GADTs, FlexibleContexts,StandaloneDeriving #-}
+{-# LANGUAGE ExistentialQuantification, ExplicitForAll, StandaloneDeriving, DuplicateRecordFields,ScopedTypeVariables,InstanceSigs, KindSignatures, GADTs, FlexibleContexts,StandaloneDeriving, TypeSynonymInstances, FlexibleInstances #-}
 
 module Main where
+import Prelude hiding (EQ)  
 import Data.Typeable
 import Data.Set as Set
 import Data.Map.Strict as Map
 import Data.Void
+import Data.List as List
    
 
 --  Terms and theories --------------------------------------
 
 data Term var ty sym en fk att gen sk where    
- Var :: (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att, Eq gen, Eq sk,
+ Var :: (Ord var, Ord ty, Ord sym, Ord en, Ord fk, Ord att, Ord gen, Ord sk,
   Show var, Show ty, Show sym, Show en, Show fk, Show att, Show gen, Show sk)
    => var -> Term var ty sym en fk att gen sk 
- Sym :: (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att, Eq gen, Eq sk,
+ Sym :: (Ord var, Ord ty, Ord sym, Ord en, Ord fk, Ord att, Ord gen, Ord sk,
   Show var, Show ty, Show sym, Show en, Show fk, Show att, Show gen, Show sk) => 
    sym -> [Term var ty sym en fk att gen sk] ->
   Term var ty sym en fk att gen sk
- Fk :: (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att, Eq gen, Eq sk,
+ Fk :: (Ord var, Ord ty, Ord sym, Ord en, Ord fk, Ord att, Ord gen, Ord sk,
   Show var, Show ty, Show sym, Show en, Show fk, Show att, Show gen, Show sk) => 
   fk -> (Term var ty sym en fk att gen sk) -> Term var ty sym en fk att gen sk
- Att :: (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att, Eq gen, Eq sk,
+ Att :: (Ord var, Ord ty, Ord sym, Ord en, Ord fk, Ord att, Ord gen, Ord sk,
   Show var, Show ty, Show sym, Show en, Show fk, Show att, Show gen, Show sk) =>
   att -> (Term var ty sym en fk att gen sk) -> Term var ty sym en fk att gen sk
- Gen :: (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att, Eq gen, Eq sk,
+ Gen :: (Ord var, Ord ty, Ord sym, Ord en, Ord fk, Ord att, Ord gen, Ord sk,
   Show var, Show ty, Show sym, Show en, Show fk, Show att, Show gen, Show sk) => 
   gen -> Term var ty sym en fk att gen sk 
- Sk :: (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att, Eq gen, Eq sk,
+ Sk :: (Ord var, Ord ty, Ord sym, Ord en, Ord fk, Ord att, Ord gen, Ord sk,
   Show var, Show ty, Show sym, Show en, Show fk, Show att, Show gen, Show sk) => 
   sk -> Term var ty sym en fk att gen sk 
   
-deriving instance Eq (Term var ty sym en fk att gen sk)
-deriving instance Show (Term var ty sym en fk att gen sk)
+instance Show (Term var ty sym en fk att gen sk) where
+ show x = case x of
+           Var v -> show v
+           Gen g -> show g
+           Sk  s -> show s
+           Fk  fk  a  -> (show a) ++ "." ++ (show fk)
+           Att att a  -> (show a) ++ "." ++ (show att)
+           Sym sym az -> (show sym) ++ "(" ++ (intercalate "," $ List.map show az) ++ ")" 
 
---deriving instance Eq a => Eq (Expr a)
---deriving instance Show a => Show (Expr a)
- --deriving (Eq, Show) --todo: pretty printer
- 
+
+deriving instance Eq (Term var ty sym en fk att gen sk)
+deriving instance Ord (Term var ty sym en fk att gen sk)
+
 type Ctx k v = Map k v --todo: wrap Map class to throw an error if a key is ever put twice
  
-type EQ var ty sym en fk att gen sk 
- = (Term var ty sym en fk att gen sk, Term var ty sym en fk att gen sk)
+newtype EQ var ty sym en fk att gen sk 
+ = EQ (Term var ty sym en fk att gen sk, Term var ty sym en fk att gen sk)
+ deriving Eq
+ 
+instance Show (EQ var ty sym en fk att gen sk) where
+ show (EQ (lhs,rhs)) = ((show lhs) ++ " = " ++ (show rhs))
 
 data Collage var ty sym en fk att gen sk = Collage {
- ctx :: Ctx var (Either ty en)
- , eqs :: EQ var ty sym en fk att gen sk
- , tys :: Set ty
+   ctx  :: Ctx var (Either ty en)
+ , eqs  :: EQ var ty sym en fk att gen sk
+ , tys  :: Set ty
  , ens  :: Set en
  , fks  :: Map fk (en, en)
  , atts :: Map att (en, ty)
  , gens :: Map gen en
- , sks :: Map sk ty }
+ , sks  :: Map sk ty }
  deriving (Eq, Show)
  
-typeOf :: (Ord var, Show var, Show en, Show ty) 
-  => Collage var ty sym en fk att gen sk -> 
+typeOf :: Collage var ty sym en fk att gen sk -> 
   Term var ty sym en fk att gen sk -> Either String (Either ty en)
 typeOf col (Var var) = case Map.lookup var (ctx col) of 
-    Nothing -> Left $ "Cannot find " ++ (show var) ++ " in " ++ (show $ ctx col)  
-    Just v  -> return v
---todo: complete typeOf   
-    
+    Nothing -> Left $ "Cannot find variable " ++ (show var) ++ " in " ++ (show $ ctx col)  
+    Just v  -> return v    
+typeOf _ _ = undefined     
 
 --  Semantics -----------------------------------------------------------------
 
@@ -73,20 +83,27 @@ data Typeside var ty sym = Typeside {
   {-- since we're in Haskell, a different DSL embedding strategy might be called for than the java version
 , hakell_tys :: Map ty String
 , haskell_syms :: Map sym String --}
-} --todo: eq, show
+} 
+
+typesideToCollage :: Typeside var ty sym 
+  -> Collage var ty sym Void Void Void Void Void 
+typesideToCollage = undefined -- todo  
+
 
 instance (Eq var, Eq ty, Eq sym) => Eq (Typeside var ty sym) where 
  Typeside tys syms eqs eq == Typeside tys' syms' eqs' eq' =
   (tys == tys') && (syms == syms') && (eqs == eqs')
-
-typesideDom = Typeside (Set.singleton "Dom") sym (Set.empty) (\ctx (lhs,rhs) -> lhs == rhs)
+instance (Show var, Show ty, Show sym) => Show (Typeside var ty sym) where
+ show (Typeside tys syms eqs eq) = "tys = " ++ (show tys) ++ 
+   "\nsyms = " ++ (show syms) ++ "\neqs = " ++ (show eqs)
+ 
+--example typeside one sort Dom { c0 ,..., c100 }
+typesideDom = Typeside (Set.singleton "Dom") sym (Set.empty) (\ctx (EQ (lhs,rhs)) -> lhs == rhs)
  where sym = sym' 100
        sym' 0 = Map.empty 
        sym' n = Map.insert ("c" ++ (show n)) ([], "Dom") $ sym' (n-1)
-  
-typesideToCollage :: Typeside var ty sym 
-  -> Collage var ty sym Void Void Void Void Void 
-typesideToCollage = undefined -- todo  
+
+-------------------
 
 data Schema var ty sym en fk att = Schema {
     typeside :: Typeside var ty sym
@@ -96,10 +113,7 @@ data Schema var ty sym en fk att = Schema {
   , path_eqs :: Set (en, EQ () Void Void en fk Void Void Void)
   , obs_eqs  :: Set (en, EQ () ty sym en fk att Void Void)
   , eq :: en -> EQ () ty sym en fk att Void Void -> Bool
-} --todo: show
-
---todo: ask erik
---todo: why doesn't thing compile?
+} 
 
 instance (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att)
   => Eq (Schema var ty sym en fk att) where
@@ -109,17 +123,48 @@ instance (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att)
     && (path_eqs == path_eqs') && (obs_eqs == obs_eqs')
     && (ts == ts')
     
-
-schemaOne = Schema typesideDom (Set.singleton "A") Map.empty
- atts' Set.empty Set.empty (\en (lhs, rhs) -> lhs == rhs)
- where atts' = Map.insert "A_att" ("A", "Dom") Map.empty
-
-schemaTwo = Schema typesideDom (Set.union (Set.singleton "A") (Set.singleton "B")) (Map.insert "f" ("A", "B") Map.empty) atts' Set.empty Set.empty (\en (lhs, rhs) -> lhs == rhs)
-  where atts' = Map.insert "A_att" ("A", "Dom") $ Map.insert "B_att" ("B", "Dom") Map.empty
+instance (Show var, Show ty, Show sym, Show en, Show fk, Show att)
+  => Show (Schema var ty sym en fk att) where
+ show (Schema ts ens fks atts path_eqs obs_eqs eq) = 
+   "ens = " ++ (show ens) ++ 
+   "\nfks = " ++ (show fks) ++ "\natts = " ++ (show atts) ++
+   "\npath_eqs = " ++ (show path_eqs) ++ "\nobs_eqs = " ++ (show obs_eqs)
 
 schemaToCollage :: Schema var ty sym en fk att
   -> Collage var ty sym en fk att Void Void
 schemaToCollage = undefined -- todo  
+
+schemaOne = Schema typesideDom (Set.singleton "A") Map.empty
+ atts' Set.empty Set.empty (\en (EQ (lhs, rhs)) -> lhs == rhs)
+ where atts' = Map.insert "A_att" ("A", "Dom") Map.empty
+
+schemaTwo = Schema typesideDom (Set.union (Set.singleton "A") (Set.singleton "B")) (Map.insert "f" ("A", "B") Map.empty) atts' Set.empty Set.empty (\en (EQ (lhs, rhs)) -> lhs == rhs)
+  where atts' = Map.insert "A_att" ("A", "Dom") $ Map.insert "B_att" ("B", "Dom") Map.empty
+
+-------------
+
+data Algebra var ty sym en fk att gen sk x y = Algebra {
+   schemaA :: Schema var ty sym en fk att 
+  
+  , ens  :: Map en (Set x)
+  , fks  :: Map fk (Map x x)
+  , atts :: Map att (Map x (Term Void ty sym Void Void Void Void y))
+   
+  , nf :: Term Void Void Void en fk Void gen Void -> x
+  , repr :: x -> Term Void Void Void en fk Void gen Void
+  
+  , nf' :: Term var ty sym en fk att gen sk -> 
+           Term Void ty sym Void Void Void Void y
+         
+  , repr' :: Term Void ty sym Void Void Void Void y ->
+             Term var ty sym en fk att gen sk
+} -- omit Eq, doesn't seem to be necessary 
+    
+instance (Show var, Show ty, Show sym, Show en, Show fk, Show att, Show gen, Show sk, Show x, Show y)
+  => Show (Algebra var ty sym en fk att gen sk x y) where
+ show (Algebra _ ens fks atts nf repr nf' repr') = 
+   "ens = " ++ (show ens) ++ 
+   "\nfks = " ++ (show fks) ++ "\natts = " ++ (show atts)
 
 data Instance var ty sym en fk att gen sk x y = Instance {
     schema :: Schema var ty sym en fk att 
@@ -130,73 +175,114 @@ data Instance var ty sym en fk att gen sk x y = Instance {
     
   , algebra :: Algebra var ty sym en fk att gen sk x y    
 
+} 
 
-} --todo: Eq and Show instances
+ 
+instance (Show var, Show ty, Show sym, Show en, Show fk, Show att, Show gen, Show sk, Show x, Show y)
+  => Show (Instance var ty sym en fk att gen sk x y) where
+ show (Instance _ gens sks eqs eq _) = 
+   "gens = " ++ (show gens) ++ 
+   "\nsks = " ++ (show sks) ++ "\neqs = " ++ (show eqs)
+
+instance (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att, Eq gen, Eq sk, Eq x, Eq y)
+  => Eq (Instance var ty sym en fk att gen sk x y) where
+ (Instance schema gens sks eqs _ _) == (Instance schema' gens' sks' eqs' _ _)
+  = (schema == schema') && (gens == gens') && (sks == sks') && (eqs == eqs') 
+
 
 validate :: Instance var ty sym en fk att gen sk x y
  -> Algebra var ty sym en fk att gen sk x y -> Either String ()
 validate _ _ = Left "todo" --todo
-
-data Algebra var ty sym en fk att gen sk x y = Algebra {
-   schemaA :: Schema var ty sym en fk att 
-  
-  , ens  :: en -> Set x
-  , fks  :: fk -> x -> x
-  , atts :: att -> x -> Term Void ty sym Void Void Void Void y
-   
-  , nf :: Term Void Void Void en fk Void gen Void -> x
-  , repr :: x -> Term Void Void Void en fk Void gen Void
-  
-  , nf' :: Term var ty sym en fk att gen sk -> 
-           Term Void ty sym Void Void Void Void y
-         
-  , repr' :: Term Void ty sym Void Void Void Void y ->
-             Term var ty sym en fk att gen sk
-} --todo Show
-
-
-
-instanceOne = Instance schemaOne 
- (Map.insert "g1" "A" Map.empty) Map.empty  Set.empty
-  (\(lhs,rhs) -> lhs == rhs) $ Algebra schemaOne (\_ -> Set.singleton "x") 
-   (\_ -> undefined) (\_ _ -> Sym "c42" [])
-   (\t -> "x") (\t -> Gen "g1") (\t -> undefined) (\t -> undefined)
 
 
 instanceToCollage :: Instance var ty sym en fk att gen sk x y
   -> Collage var ty sym en fk att gen sk
 instanceToCollage = undefined -- todo  
 
+instanceOne = Instance schemaOne 
+ (Map.insert "g1" "A" Map.empty) Map.empty  Set.empty (\(EQ (lhs,rhs)) -> lhs == rhs)
+ $ Algebra schemaOne (Map.fromList [("A", Set.singleton "x")]) 
+   (Map.empty) (Map.fromList [("A_att", Map.fromList [("x",Sym "c42" [])])])
+   (\t -> "x") (\t -> Gen "g1") (\t -> Sym "c42" []) (\t -> Sym "c42" [])
+
+
+-------------
+
 data Mapping var ty sym en fk att en' fk' att' = Mapping {
-    src :: Schema var ty sym en fk att
+    src :: Schema var ty sym en  fk  att
   , dst :: Schema var ty sym en' fk' att'
   
-  , ens  :: Map en en'
+  , ens  :: Map en  en'
   , fks  :: Map fk  (Term () Void Void en' fk' Void Void Void)
   , atts :: Map att (Term () ty   sym  en' fk' att' Void Void)
-} --todo: Eq and Show instances
+} 
+
+instance (Show var, Show ty, Show sym, Show en, Show fk, Show att, Show en', Show fk', Show att')
+  => Show (Mapping var ty sym en fk att en' fk' att') where
+ show (Mapping _ _ ens fks atts) = 
+   "ens = " ++ (show ens) ++ 
+   "\nfks = " ++ (show fks) ++ "\natts = " ++ (show atts)
+
+instance (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att, Eq en', Eq fk', Eq att')
+  => Eq (Mapping var ty sym en fk att en' fk' att') where
+ (Mapping s1 s2 ens fks atts) == (Mapping s1' s2' ens' fks' atts')
+  = (s1 == s1') && (s2 == s2') && (ens == ens') && (fks == fks') && (atts == atts') 
+
 
 mappingTwoToOne = Mapping schemaTwo schemaOne
  (Map.fromList [("B", "A"), ("A","A")])
  (Map.fromList [("f", Var ())])
  (Map.fromList [("A_att", Att "att" (Var ())), ("B_att", Att "att" (Var ()))])
 
-data Transform var ty sym en fk att gen sk x y gen' sk' x' y' 
- = Transform { 
-    srcT  :: Instance var ty sym en fk att gen sk x y
-  , dstT  :: Instance var ty sym en fk att gen' sk' x' y'
-  , gens :: Map gen (Term Void Void Void en fk Void gen' Void)
-  , tys  :: Map ty  (Term var  ty   sym  en fk att  gen' sk') 
-} --todo: Eq and Show instances
-  
+-----
+
 data Query var ty sym en fk att en' fk' att' = Query {
     srcQ :: Schema var ty sym en fk att
   , dstQ :: Schema var ty sym en' fk' att'
   
   , ens  :: Map en' (Ctx var en, Set (EQ var ty sym en fk att Void Void))
-  , fks  :: Map fk'  (Term var Void Void en fk Void Void Void)
+  , fks  :: Map fk'  (Ctx var (Term var Void Void en fk Void Void Void))
   , atts :: Map att' (Term var ty   sym  en fk att  Void Void)
-} --todo: Eq and Show instances
+} 
+
+instance (Show var, Show ty, Show sym, Show en, Show fk, Show att, Show en', Show fk', Show att')
+  => Show (Query var ty sym en fk att en' fk' att') where
+ show (Query _ _ ens fks atts) = 
+   "ens = " ++ (show ens) ++ 
+   "\nfks = " ++ (show fks) ++ "\natts = " ++ (show atts)
+
+instance (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att, Eq en', Eq fk', Eq att')
+  => Eq (Query var ty sym en fk att en' fk' att') where
+ (Query s1 s2 ens fks atts) == (Query s1' s2' ens' fks' atts')
+  = (s1 == s1') && (s2 == s2') && (ens == ens') && (fks == fks') && (atts == atts') 
+
+
+------
+
+
+data Transform var ty sym en fk att gen sk x y gen' sk' x' y' 
+ = Transform { 
+    srcT  :: Instance var ty sym en fk att gen sk x y
+  , dstT  :: Instance var ty sym en fk att gen' sk' x' y'
+  , gens :: Map gen (Term Void Void Void en fk Void gen' Void)
+  , sks  :: Map sk  (Term var  ty   sym  en fk att  gen' sk') 
+} 
+
+
+instance (Show var, Show ty, Show sym, Show en, Show fk, Show att, Show gen, Show sk, Show x, Show y, Show gen', Show sk', Show x', Show y')
+  => Show (Transform var ty sym en fk att gen sk x y gen' sk' x' y') where
+ show (Transform _ _ gens sks) = 
+   "gens = " ++ (show gens) ++ 
+   "\nsks = " ++ (show sks) 
+
+instance (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att, Eq gen, Eq sk, Eq x, Eq y, Eq gen', Eq sk', Eq x', Eq y')
+  => Eq (Transform var ty sym en fk att gen sk x y gen' sk' x' y') where
+ (Transform s1 s2 gens sks) == (Transform s1' s2' gens' sks')
+  = (s1 == s1') && (s2 == s2') && (gens == gens') && (sks == sks') 
+
+
+  
+----
 
 -- Syntax ------------------------------------------
 
@@ -210,7 +296,6 @@ evalTypeside :: TypesideExp var ty sym -> Either String (Typeside var ty sym)
 evalTypeside (TypesideLiteral typeside) = return typeside
 evalTypeside  TypesideInitial = return $ Typeside Set.empty Map.empty Set.empty undefined -- todo: replace undefined with non effectful code 
 
--- todo: colimits, etc
 data SchemaExp :: * -> * -> * -> * -> * -> * -> * where 
   SchemaLiteral :: Schema var ty sym en fk att -> SchemaExp var ty sym en fk att
   SchemaInitial :: Typeside var ty sym -> SchemaExp var ty sym en fk att
@@ -227,7 +312,7 @@ evalSchema (SchemaInitial typeside) = return schema
 evalSchema (SchemaCoProd s1 s2) = Left "todo"
 --todo: additional schema functions
 
--- todo: colimits, etc
+
 data InstanceExp :: * -> * -> * -> * -> * -> * -> * -> * -> * -> * -> * where
   InstanceLiteral :: Instance var ty sym en fk att gen sk x y ->
     InstanceExp var ty sym en fk att gen sk x y
@@ -235,7 +320,7 @@ data InstanceExp :: * -> * -> * -> * -> * -> * -> * -> * -> * -> * -> * where
     InstanceExp var ty sym en fk att Void Void Void Void
  
   InstanceDelta :: 
-   (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att, Eq gen, Eq sk, Eq x, Eq y,
+   (Ord var, Ord ty, Ord sym, Ord en, Ord fk, Ord att, Ord gen, Ord sk, Eq x, Eq y,
     Eq en', Eq fk', Eq att') =>
    MappingExp var ty sym en fk att en' fk' att'  
    ->  InstanceExp var ty sym en' fk' att' gen sk x y
@@ -253,17 +338,11 @@ data InstanceExp :: * -> * -> * -> * -> * -> * -> * -> * -> * -> * -> * where
   InstanceCoEval :: MappingExp var ty sym en fk att en' fk' att'  
    ->  InstanceExp var ty sym en fk att gen' sk' x' y'
    ->  InstanceExp var ty sym en fk att gen sk x y
-  {--
- schema :: Schema var ty sym en fk att 
-  , gens :: Map gen en
-  , sks :: Map sk ty
-  , eqs :: Set (EQ Void ty sym en fk att gen sk)             
-  , eq :: EQ var ty sym en fk att gen sk -> Bool
---}
+  
 
 
 evalDeltaAlgebra ::
- (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att, Eq gen, Eq sk, Eq x, Eq y,
+ (Ord var, Ord ty, Ord sym, Ord en, Ord fk, Ord att, Ord gen, Ord sk, Eq x, Eq y,
     Eq en', Eq fk', Eq att') =>
    Mapping var ty sym en fk att en' fk' att'  
    ->  Instance var ty sym en' fk' att' gen sk x y
@@ -271,7 +350,7 @@ evalDeltaAlgebra ::
 evalDeltaAlgebra = undefined --todo
 
 evalDeltaInst ::
- (Eq var, Eq ty, Eq sym, Eq en, Eq fk, Eq att, Eq gen, Eq sk, Eq x, Eq y,
+ (Ord var, Ord ty, Ord sym, Ord en, Ord fk, Ord att, Ord gen, Ord sk, Eq x, Eq y,
     Eq en', Eq fk', Eq att') =>
    Mapping var ty sym en fk att en' fk' att'  
    ->  Instance var ty sym en' fk' att' gen sk x y
@@ -290,16 +369,15 @@ evalInstance (InstanceDelta f' i') =
       
 
  
---todo: show instances
+
 evalInstance (InstanceLiteral inst) = return inst  
 evalInstance (InstanceInitial schema) = return $ Instance schema 
  Map.empty Map.empty Set.empty undefined $ Algebra schema 
- (\_ -> Set.empty) undefined undefined
+ (Map.empty) undefined undefined
  undefined undefined undefined undefined 
  --todo: undefineds can be replaced with actually non effectful code
 evalInstance _ = Left "todo"
 
---todo: compose
 data MappingExp :: * -> * -> * -> * -> * -> * -> * -> * -> * -> * where
  MappingId :: SchemaExp var ty sym en fk att ->
    MappingExp var ty sym en fk att en fk att
@@ -311,7 +389,6 @@ evalMapping :: MappingExp var ty sym en fk att en' fk' att' ->
 evalMapping (MappingId schema) = Left "todo" --todo
 evalMapping (MappingLiteral l) = return l 
 
--- todo: compose
 data QueryExp :: * -> * -> * -> * -> * -> * -> * -> * -> * -> * where
  QueryId :: SchemaExp var ty sym en fk att ->
    QueryExp var ty sym en fk att en fk att
@@ -323,7 +400,6 @@ evalQuery :: QueryExp var ty sym en fk att en' fk' att'
 evalQuery (QueryId schema) = Left "todo" --todo
 evalQuery (QueryLiteral q) = return q
 
--- todo: compose
 data TransformExp :: * -> * -> * -> * -> * -> * -> * -> * -> * -> * -> * -> * -> * -> * -> * where 
  TransformId :: InstanceExp var ty sym en fk att gen sk x y ->
    TransformExp var ty sym en fk att gen sk x y gen sk x y 
@@ -377,4 +453,21 @@ evalTransform (TransformLiteral h) = return h
 
 
 
-main = putStrLn "hio"
+main = do putStrLn $ show typesideDom
+          putStrLn $ show schemaOne
+          putStrLn $ show schemaTwo
+          putStrLn $ show mappingTwoToOne
+          putStrLn $ show instanceOne
+
+
+
+
+
+
+
+
+
+
+
+
+
