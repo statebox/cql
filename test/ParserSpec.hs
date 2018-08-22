03d6ef0cd9616c8ae7f4cfdc88ce39b77325f6db
Marco Perone <pasafama@gmail.com>
2018-08-22 19:21:17 +0200

    parse a complete schema #6

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -11,4 +11,34 @@ import           Text.Megaparsec
 spec :: Spec
 spec = do
     it "parses correctly a schema" $
-        parse statement "" "Schema" == Right "Schema"
+        parse
+            schemaParser
+            ""
+            "Schema S = literal : Ty { \
+            \    entities \
+            \        Person \
+            \        Company \
+            \        CompanyPerson \
+            \    foreign_keys \
+            \        persons_id   : CompanyPerson -> Person \
+            \        companies_id : CompanyPerson -> Company \
+            \    attributes \
+            \        firstname lastname : Person  -> String \
+            \        name      : Company -> String \
+            \}"
+            == Right (
+                SchemaStmt
+                    "S"
+                    "Ty"
+                    ( SchemaDef
+                        [ EntityDef "Person"
+                        , EntityDef "Company"
+                        , EntityDef "CompanyPerson"
+                        ]
+                        [ ForeignKeyDef "persons_id" "CompanyPerson" "Person"
+                        , ForeignKeyDef "companies_id" "CompanyPerson" "Company"
+                        ]
+                        [ AttributeDef ["firstname", "lastname"] "Person" "String"
+                        , AttributeDef ["name"] "Company" "String"]
+                    )
+                )
