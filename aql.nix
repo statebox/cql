ee3caf8908c340aec893e6b967fb13019bf8d055
TG x <*@tg-x.net>
2018-09-26 14:25:14 +0200

    nix: fix build

--- aql.nix
+++ aql.nix
@@ -1,14 +1,22 @@
-{ mkDerivation, base, containers, hspec, megaparsec, stdenv }:
+{ mkDerivation, stdenv, hpack, base, containers, hspec, megaparsec }:
 mkDerivation {
   pname = "aql";
   version = "0.1.0.0";
   src = ./.;
+
   isLibrary = true;
   isExecutable = true;
   doCheck = true;
+
+  buildDepends = [ hpack ];
   libraryHaskellDepends = [ base containers megaparsec ];
   executableHaskellDepends = [ base containers megaparsec ];
   testHaskellDepends = [ base containers hspec megaparsec ];
+
+  preConfigure = ''
+    hpack
+  '';
+
   homepage = "https://github.com/statebox/aql#readme";
   license = stdenv.lib.licenses.bsd3;
 }
