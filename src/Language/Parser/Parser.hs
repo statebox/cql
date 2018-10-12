2b6e22bc78cb20857105bda7903ea6c832197331
Marco Perone <pasafama@gmail.com>
2018-10-12 18:46:47 +0200

    allow spaces in identifiers between quotes #64

--- src/Language/Parser/Parser.hs
+++ src/Language/Parser/Parser.hs
@@ -37,7 +37,7 @@ identifier :: Parser String
 identifier = (lexeme . try) (p' >>= check)
   where
     p = lowerId <|> upperId <|> specialId
-    p' = p <|> between (char '"') (char '"') p
+    p' = p <|> between (char '"') (char '"') (unwords <$> some (lexeme p))
     check x =
       if x `elem` reservedWords
         then fail $ "keyword" ++ show x ++ "cannot be used as an identifier"
