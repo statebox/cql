3bcd5a81af577d088586afdf6c7d1bea9a588969
Marco Perone <pasafama@gmail.com>
2018-10-19 08:31:12 +0200

    starting mapping composition #25

--- src/Language/Mapping.hs
+++ src/Language/Mapping.hs
@@ -116,6 +116,25 @@ trans'' _ (Att _ _) = undefined
 trans'' _ (Gen (_,g)) = Gen g
 trans'' _ _ = undefined
 
+compose :: (Eq var, Eq ty, Eq sym, Eq en', Eq fk', Eq att') =>
+  Mapping var ty sym en  fk  att  en'  fk'  att'  ->
+  Mapping var ty sym en' fk' att' en'' fk'' att'' ->
+  Err (Mapping var ty sym en  fk  att  en'' fk'' att'')
+compose (Mapping src' dst' ens' fks' atts') (Mapping src'' dst'' ens'' fks'' atts'') =
+  if dst' == src''
+  then Right $ Mapping src' dst'' ens''' fk''' atts'''
+  else Left "The target of the first mapping does not coincide with the source of the second"
+    where
+    --ens''' :: Map en en''
+      ens''' = fmap (\en -> Map.lookup en ens'') ens' -- TODO: compose ens correctly
+
+    --fk''' :: Map fk (Term () Void Void en'' fk'' Void Void Void)
+      fk''' = undefined
+
+    --atts''' :: Map att (Term () ty sym en'' fk'' att'' Void Void)
+      atts''' = undefined
+
+
 data MappingExp   where
   MappingVar     :: String -> MappingExp
   MappingId      :: SchemaExp -> MappingExp
